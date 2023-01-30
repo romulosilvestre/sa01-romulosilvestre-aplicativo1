eb5e890eecbc273b1642afc35b99ea2221108485
romulo.silvestre <romulo.silvestre@df.estudante.senai.br>
2023-01-30 16:45:37 -0300

    criei o método média bimestral

--- src/br/com/senaitagua/sa1/app1/Principal.java
+++ src/br/com/senaitagua/sa1/app1/Principal.java
@@ -5,23 +5,22 @@ import br.com.senaitagua.sa1.bo.CalculoBO;
 public class Principal { //classe
 
 	//método main - é um método principal para executar o programa.
-	public static void main(String[] args) { // chave que abre - inicio
-		
+	public static void main(String[] args) { // chave que abre - inicio		
 		// TODO Auto-generated method stub
 		double valor1,valor2;
-		JOptionPane.showMessageDialog(null,"Empresa Voe - Grupo Empresarial");
-		
+		JOptionPane.showMessageDialog(null,"Empresa Voe - Grupo Empresarial");		
 		//Entrada de Dados
 		valor1 = Double.parseDouble(JOptionPane.showInputDialog("digite o valor do primeiro avião"));
-		valor2 = Double.parseDouble(JOptionPane.showInputDialog("digite o valor do segundo avião"));
-		
+		valor2 = Double.parseDouble(JOptionPane.showInputDialog("digite o valor do segundo avião"));		
 		//Passar uma mensagem (método) para o objeto
-		CalculoBO cbo = new CalculoBO();
-		
-		//utilizar o objeto
-		
-		
-		JOptionPane.showMessageDialog(null,cbo.somar(valor1,valor2));
+		CalculoBO cbo = new CalculoBO();		
+		//utilizar o objeto			
+		JOptionPane.showMessageDialog(null,cbo.somar(valor1,valor2));		
+		String resp1 = JOptionPane.showInputDialog("Digite o fat. Jan");
+		String resp2 = JOptionPane.showInputDialog("Digite o fat. Fev");
+		double valorJan = Double.parseDouble(resp1);
+		double valorFev = Double.parseDouble(resp2);
+		JOptionPane.showMessageDialog(null,"Média Bimestral Fat:"+cbo.calcularMedia(valorJan, valorFev));	
 
 	} //chave que fecha - fim
 
--- src/br/com/senaitagua/sa1/bo/CalculoBO.java
+++ src/br/com/senaitagua/sa1/bo/CalculoBO.java
@@ -1,9 +1,12 @@
 package br.com.senaitagua.sa1.bo;
 
 public class CalculoBO {
-
+    
 	public double somar(double n1,double n2) {
 		return n1+n2;
+	}
+	public double calcularMedia(double valor1,double valor2) {
+		return (valor1+valor2)/2;
 	}
 }
 
